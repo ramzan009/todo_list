1a9e34dc28e86b9015fa93ef8d3b8d8e9dde0725
Дикаев Рахман Расулович <rahman.corporative@gmail.com>
2025-06-12 16:02:01 +0300

    Создал миграции
    1. Создал модели, и писал отношения
    2. Исправил косяки

---
+++ database/migrations/2025_06_12_112518_create_attachments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('task_id');
+
+            $table->string('file_path');
+            $table->string('file_name');
+            $table->enum('file_type', ['image', 'video', 'audio', 'document']);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('task_id')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attachments');
+    }
+};
